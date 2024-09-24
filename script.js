7f6ad307c0a9d12e05a377c3f0a6f1e1e4084477
Christina <206peacegypsy@gmail.com>
2024-09-24 11:05:10 -0700

    contains two methods to complete assignment

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let Me = {
+/*let Me = {
     firstName: "Christina",
     lastName: "Sherman",
     yearsOfStudy: 2,
     goal: "to graduate with a degree in cybersecurity."
-};
+}; */
+
+const firstName = "Christina";
+const lastName = "Sherman";
+const yearsOfStudy = 2;
+let goal = "to graduate with a degree in cybersecurity.";
+const words = `Hello! My name is ${firstName} ${lastName}.<br /> I have been studying at SCC for ${yearsOfStudy} years.<br /> After completing my courses I hope to ${goal}`;
