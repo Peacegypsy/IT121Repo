efaf4907560b6fd3e30e61132cfc8e7b376978ae
christina <christina.sherman@seattlecolleges.edu>
2024-10-16 13:23:03 -0700

    adjusting per assignment

---
+++ week1/intro.js
@@ -0,0 +1,6 @@
+
+const firstName = "Christina";
+const lastName = "Sherman";
+const yearsOfStudy = 2;
+let goal = "to graduate with a degree in cybersecurity.";
+const words = `Hello! My name is ${firstName} ${lastName}.\n I have been studying at SCC for ${yearsOfStudy} years.\n After completing my courses I hope to ${goal}`;
